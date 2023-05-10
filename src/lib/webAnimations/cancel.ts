8a5357740a3d978a597e98747a32a6c8d1a92a4f
‘YAG’ <ioingnet@gmail.com>
2023-05-10 20:39:36 +0800

    fix: animation clear

--- src/lib/webAnimations/cancel.ts
+++ src/lib/webAnimations/cancel.ts
@@ -3,11 +3,11 @@ export default (element: HTMLElement) => {
     animation.finish()
     animation.cancel()
   })
-  const computedStyle = window.getComputedStyle(element)
+  const currentStyle = element.style
   const styleObject: { [key: string]: string } = {}
-  for (let i = 0; i < computedStyle.length; i++) {
-    const propName = computedStyle[i]
-    const propValue = computedStyle.getPropertyValue(propName)
+  for (let i = 0; i < currentStyle.length; i++) {
+    const propName = currentStyle[i]
+    const propValue = currentStyle.getPropertyValue(propName)
     styleObject[propName] = propValue
   }
   const restAnimate = element.animate(styleObject, {

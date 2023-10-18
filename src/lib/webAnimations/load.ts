c285e273b5174d22d8f2272771f7f8efbd633cea
‘YAG’ <ioingnet@gmail.com>
2023-10-18 10:27:15 +0800

    feat: check getAnimations

--- src/lib/webAnimations/load.ts
+++ src/lib/webAnimations/load.ts
@@ -1,5 +1,5 @@
 export default async (): Promise<boolean> => {
-  if (!('animate' in document.createElement('div'))) {
+  if (!('animate' in document.createElement('div')) || !Element.prototype.getAnimations) {
     try {
       return !!(await import('./polyfill')).default
     } catch (e) {
